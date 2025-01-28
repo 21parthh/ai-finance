49947904db6026641d8720c06c02594b450b832e
Parth Deore <parth.deore@icloud.com>
2025-01-28 20:57:48 +0530

    data models are created and now user getting stored

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,53 +1,58 @@
-"use client"
-
-import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/clerk-react";
-import {Button} from "@/components/ui/button";
+import React from "react";
+import { PenBox, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
-import Toggle from "@/components/ThemeProvider/toggle";
-import {LayoutDashboard, PenBox} from "lucide-react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
+import { checkUser } from "@/lib/checkUser";
+import { Button } from "../ui/button";
 import {stringify} from "@/constants/stringify";
 
-export default function Index() {
+
+const Header = async () => {
+    await checkUser();
+
     return (
-        <header className="fixed top-0 w-full bg-transparent backdrop-blur-md z-50">
+        <header className="fixed top-0 w-full bg-transparent text-white backdrop-blur-md z-50 border-b">
             <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
                 <Link href="/" className="h-12 w-auto flex items-center text-lg font-bold">
                     {stringify.logoTitle}
                 </Link>
-                {/* Right Section (Buttons for Sign In/Sign Up or User Account) */}
                 <div className="flex items-center space-x-4">
-                    <Toggle/>
-                    <SignedOut>
-                        <SignInButton>
-                            <Button variant="ghost">{stringify.login}</Button>
-                        </SignInButton>
-                        <SignUpButton>
-                            <Button>{stringify.signUp}</Button>
-                        </SignUpButton>
-                    </SignedOut>
                     <SignedIn>
-                        <Link href="/dashboard" className={"text-gray-600 dark:text-white hover:text-blue-600 flex items-center gap-2"}>
-                           <Button variant="ghost">
-                               <LayoutDashboard size={80}/>
-                               <span className={"hidden md:inline"}>{stringify.dashboard}</span>
-                           </Button>
-                        </Link>
-                        <Link href="/transacion/create" className={"text-gray-600 hover:text-blue-600 flex items-center gap-2"}>
-                            <Button >
-                                <PenBox size={80}/>
-                                <span className={"hidden md:inline"}>{stringify.transactions}</span>
+                        <Link
+                            href="/dashboard"
+                            className="text-white hover:text-blue-600 flex items-center gap-2"
+                        >
+                            <Button variant="outline">
+                                <LayoutDashboard size={18} />
+                                <span className="hidden md:inline">Dashboard</span>
                             </Button>
                         </Link>
-                        <UserButton appearance={{
-                            elements: {
-                                avatarBox: "w-10 h-10",
-                            }
-                        }}/>
+                        <a href="/transaction/create">
+                            <Button className="flex items-center gap-2">
+                                <PenBox size={18} />
+                                <span className="hidden md:inline">Add Transaction</span>
+                            </Button>
+                        </a>
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton forceRedirectUrl="/dashboard">
+                            <Button variant="outline">Login</Button>
+                        </SignInButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton
+                            appearance={{
+                                elements: {
+                                    avatarBox: "w-10 h-10",
+                                },
+                            }}
+                        />
                     </SignedIn>
                 </div>
             </nav>
         </header>
+    );
+};
 
-    )
-}
+export default Header;
