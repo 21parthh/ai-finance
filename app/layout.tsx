c6e08ead1165da0cc13621dd60559ea2f1b5f660
Parth Deore <parth.deore@icloud.com>
2025-01-29 12:47:26 +0530

    feature: account creation feature s done

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import {ThemeProvider} from "@/components/ThemeProvider/theme-provider";
 import Index from "@/components/header";
 import "./globals.css";
 import Footer from "@/components/footer";
+import {Toaster} from "sonner";
 
 const inter = Inter({subsets: ["latin"]})
 
@@ -32,6 +33,7 @@ export default function RootLayout({
                 disableTransitionOnChange>
                 <Index/>
                 <main className={"min-h-screen"}>{children}</main>
+                <Toaster richColors/>
                 <Footer/>
             </ThemeProvider>
             </body>
