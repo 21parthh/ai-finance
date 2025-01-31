7f1ee699727b8950a854a098a87ef04944d6701c
Parth Deore <parth.deore@icloud.com>
2025-01-31 20:10:19 +0530

    implemented sorting and deleting function

--- app/(main)/account/_components/finace-table.tsx
+++ app/(main)/account/_components/finace-table.tsx
@@ -6,7 +6,7 @@ import {format} from "date-fns";
 import {categoryColors} from "@/data/category";
 import {Badge} from "@/components/ui/badge";
 import {Tooltip, TooltipProvider, TooltipTrigger, TooltipContent} from "@/components/ui/tooltip";
-import {ChevronDown, ChevronUp, Clock, MoreHorizontal, RefreshCw} from "lucide-react";
+import {ChevronDown, ChevronUp, Clock, MoreHorizontal, RefreshCw, Search, Trash, X} from "lucide-react";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -16,7 +16,13 @@ import {
 } from "@/components/ui/dropdown-menu";
 import {Button} from "@/components/ui/button";
 import {useRouter} from "next/navigation";
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
+import {Input} from "@/components/ui/input";
+import {Select, SelectItem, SelectContent, SelectTrigger, SelectValue} from "@/components/ui/select";
+import useFetch from "@/hooks/use-fetch";
+import {bulkDeleteTransaction} from "@/actions/accounts";
+import {BarLoader} from "react-spinners";
+import {toast} from "sonner";
 
 const RECURRING_INTERVALS = {
     DAILY: "Daily",
@@ -29,13 +35,17 @@ const FinanceTable = ({transactions}) => {
     const router = useRouter();
 
     const [selectedIds, setSelectedIds] = useState([])
+    const [searchTerm, setSearchTerm] = useState("");
+    const [typeFilter, setTypeFilter] = useState("");
+    const [recurringFilter, setRecurringFilter] = useState("");
 
+    console.log(selectedIds)
     const handleSelect = (id) => {
-        setSelectedIds(current => current.includes(id)? current.filter(item => item!=id ) :[...current,id] );
+        setSelectedIds(current => current.includes(id) ? current.filter(item => item != id) : [...current, id]);
     }
 
     const handleSelectAll = () => {
-        setSelectedIds(current => current.length === filteredAndSortedTransactions.length ? []:filteredAndSortedTransactions.map((t) => t.id) );
+        setSelectedIds(current => current.length === filteredAndSortedTransactions.length ? [] : filteredAndSortedTransactions.map((t) => t.id));
     }
 
     const [sortConfig, setSortConfig] = useState({
@@ -43,7 +53,62 @@ const FinanceTable = ({transactions}) => {
         direction: "desc"
     })
 
-    const filteredAndSortedTransactions = transactions;
+    // const {loading: deleteLoading, fn: deleteFn, data:deleted} = useFetch(bulkDeleteTransaction())
+
+    // const handleBulkDelete =()=>{}
+
+    const filteredAndSortedTransactions = useMemo(() => {
+        let result = [...transactions];
+
+        // Apply search filter
+        if (searchTerm) {
+            const searchLower = searchTerm.toLowerCase();
+            result = result.filter((transaction) =>
+                transaction.description?.toLowerCase().includes(searchLower)
+            );
+        }
+
+        // Apply type filter
+        if (typeFilter) {
+            result = result.filter((transaction) => transaction.type === typeFilter);
+        }
+
+        // Apply recurring filter
+        if (recurringFilter) {
+            result = result.filter((transaction) => {
+                if (recurringFilter === "recurring") return transaction.isRecurring;
+                return !transaction.isRecurring;
+            });
+        }
+
+        // Apply sorting
+        result.sort((a, b) => {
+            let comparison = 0;
+
+            switch (sortConfig.field) {
+                case "date":
+                    comparison = new Date(a.date) - new Date(b.date);
+                    break;
+                case "amount":
+                    comparison = a.amount - b.amount;
+                    break;
+                case "category":
+                    comparison = a.category.localeCompare(b.category);
+                    break;
+                default:
+                    comparison = 0;
+            }
+
+            return sortConfig.direction === "asc" ? comparison : -comparison;
+        });
+        return result;
+    }, [
+        transactions,
+        searchTerm,
+        typeFilter,
+        recurringFilter,
+        sortConfig,
+    ]);
 
     const handleSort = (field) => {
         setSortConfig(current => ({
@@ -52,15 +117,94 @@ const FinanceTable = ({transactions}) => {
         }))
     };
 
+    const {
+        loading: deleteLoading,
+        fn: deleteFn,
+        data: deleted,
+    } = useFetch(bulkDeleteTransaction);
+
+    const handleBulkDelete = async () => {
+        if (
+            !window.confirm(
+                `Are you sure you want to delete ${selectedIds.length} transactions?`
+            )
+        )
+            return;
+
+        deleteFn(selectedIds);
+    };
+
+    useEffect(() => {
+        if (deleted && !deleteLoading) {
+            toast.success("Transactions deleted successfully");
+        }
+    }, [deleted, deleteLoading]);
+
+    const handleClearFilters = () => {
+        setSearchTerm("")
+        setTypeFilter("")
+        setRecurringFilter("")
+        setSelectedIds([])
+    }
+
     return (
         <div className="space-y-4">
+            {deleteLoading && (
+                <BarLoader className="mt-4" width={"100%"} color="#9333ea"/>
+            )}
+            {/*Filter*/}
+            <div className={"flex flex-col sm:flex-row gap-4"}>
+                <div className={"relative flex-1"}>
+                    <Search className={"absolute left-2 top-2.5 h-4 w-4 text-muted-foreground"}/>
+                    <Input className={"pl-8"} value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}
+                           placeholder={"Search"}/>
+                </div>
+
+                <div>
+                    <Select value={typeFilter} onValueChange={setTypeFilter}>
+                        <SelectTrigger>
+                            <SelectValue placeholder="All Types"/>
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="INCOME">Income</SelectItem>
+                            <SelectItem value="EXPENSE">Expense</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
+
+                <div>
+                    <Select value={recurringFilter} onValueChange={(value) => setRecurringFilter(value)}>
+                        <SelectTrigger className={"w-[150px]"}>
+                            <SelectValue placeholder="All Transactions"/>
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="recurring">Recurring Only</SelectItem>
+                            <SelectItem value="non-recurring">Non-Recurring Only</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
+
+                {selectedIds.length > 0 && <div>
+                    <Button className={"flex item-center gap-2"} variant={"destructive"} size={"sm"}
+                            onClick={handleBulkDelete}>
+                        <Trash className={"h-4 w-4 mr-2"}/>
+                        Delete Selected ({selectedIds.length})</Button>
+                </div>}
+
+                {(searchTerm || typeFilter || recurringFilter) && (
+                    <Button onClick={handleClearFilters} title={"Clear Filters"}><X className={"h-4 w-4"}/></Button>
+                )}
+            </div>
+
+
             {/* Transactions */}
             <div className="rounded-md border w-full flex justify-between">
                 <Table>
                     <TableHeader>
                         <TableRow>
                             <TableHead className="w-[50px]">
-                                <Checkbox onCheckedChange={handleSelectAll} checked={selectedIds.length === filteredAndSortedTransactions.length && filteredAndSortedTransactions.length > 0}/>
+                                <Checkbox onCheckedChange={handleSelectAll}
+                                          checked={selectedIds.length === filteredAndSortedTransactions.length && filteredAndSortedTransactions.length > 0}/>
                             </TableHead>
                             <TableHead className="cursor-pointer" onClick={() => handleSort("date")}>
                                 <div className="flex items-center">Date
@@ -101,8 +245,9 @@ const FinanceTable = ({transactions}) => {
                         ) : (
                             filteredAndSortedTransactions.map((transaction) => (
                                 <TableRow key={transaction.id}>
-                                    <TableCell className="font-medium"><Checkbox onCheckedChange={() => handleSelect(transaction.id)}
-                                    checked={selectedIds.includes(transaction.id)}/></TableCell>
+                                    <TableCell className="font-medium"><Checkbox
+                                        onCheckedChange={() => handleSelect(transaction.id)}
+                                        checked={selectedIds.includes(transaction.id)}/></TableCell>
                                     <TableCell>{format(new Date(transaction.date), 'PP')}</TableCell>
                                     <TableCell>{transaction.description}</TableCell>
                                     <TableCell className={"capitalize"}><span style={{
@@ -152,7 +297,7 @@ const FinanceTable = ({transactions}) => {
                                                 <DropdownMenuSeparator/>
                                                 <DropdownMenuItem
                                                     className="text-destructive"
-                                                    // onClick={() => deleteFn([transaction.id])}
+                                                    onClick={() => deleteFn([transaction.id])}
                                                 >Delete</DropdownMenuItem>
 
                                             </DropdownMenuContent>
