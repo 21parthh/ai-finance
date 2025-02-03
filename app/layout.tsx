77959c1001423cfb7bd138fed72e7b796397cc1d
Parth Deore <parth.deore@icloud.com>
2025-02-03 07:42:58 +0530

    bugfix: dark theme bug is fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,43 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
-import {ClerkProvider} from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-import {ThemeProvider} from "@/components/ThemeProvider/theme-provider";
+import { ThemeProvider } from "@/components/ThemeProvider/theme-provider";
 import Index from "@/components/header";
 import "./globals.css";
 import Footer from "@/components/footer";
-import {Toaster} from "sonner";
+import { Toaster } from "sonner";
 
-const inter = Inter({subsets: ["latin"]})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "AI Powered App",
-    description: "AI Powered App",
+      title: "AI Powered App",
+      description: "AI Powered App",
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+      children,
+}: Readonly<{
+      children: React.ReactNode;
 }>) {
-    return (
-        <ClerkProvider>
-            <html lang="en" suppressHydrationWarning>
-            <body
-                className={`${inter.className}`}
-            >
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="dark"
-                enableSystem
-                disableTransitionOnChange>
-                <Index/>
-                <main className={"min-h-screen"}>{children}</main>
-                <Toaster richColors/>
-                <Footer/>
-            </ThemeProvider>
-            </body>
-            </html>
-        </ClerkProvider>
-    );
+      return (
+            <ClerkProvider>
+                  <html lang="en" suppressHydrationWarning>
+                        <body className={`${inter.className}`}>
+                              <ThemeProvider
+                                    attribute="class"
+                                    defaultTheme="dark"
+                                    forcedTheme="dark"
+                              >
+                                    <Index />
+                                    <main className="min-h-screen">
+                                          {children}
+                                    </main>
+                                    <Toaster richColors />
+                                    <Footer />
+                              </ThemeProvider>
+                        </body>
+                  </html>
+            </ClerkProvider>
+      );
 }
